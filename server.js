b3c9267b60332951fc43bce48343847feee98ebc
pzyryanov1995 <pzyryanov1995@gmail.com>
2019-02-28 21:04:07 +0700

    fix koa cors issue

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const graphqlHTTP = require('koa-graphql');
 const schema = require('./grpahql/schema');
 const mount = require('koa-mount');
 const initDB = require('./database');
+const cors = require('@koa/cors');
 
 const app = new Koa();
 
 app.listen(9000);
 
 app.on('error', err => {
-    log.error('server error', err)
+    console.error('server error', err)
 });
+
+app.use(cors());
 
 app.use(mount('/graphql', graphqlHTTP({
     schema: schema,
